20e26ef2a69472ad48f2d7a76df495e7e8662c78
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2021-10-03 01:36:35 +0200

    Update build_setup.py

--- .buildscripts/build_setup.py
+++ .buildscripts/build_setup.py
@@ -7,4 +7,4 @@ parser.add_argument('--vdproj', help='.vdproj file of the setup project', requir
 parser.add_argument('--build-config', help='build config (Release, Debug)', required=True)
 args = parser.parse_args()
 
-os.system(f"devenv {args.sln} {args.vdproj} /Build ${{ env.build_config }}")
+os.system(f"\"C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\Common7\\IDE\\devenv.exe {args.sln} {args.vdproj} /Build ${{ env.build_config }}")
